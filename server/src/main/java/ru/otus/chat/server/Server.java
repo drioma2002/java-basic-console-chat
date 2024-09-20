1ea2fed24cd3e453e400614ee3222c09f3ae8200
{AndrewS} <{drioma2002@mail.ru}>
2024-09-20 18:14:25 +0300

    homework16 Init commit

--- server/src/main/java/ru/otus/chat/server/Server.java
+++ server/src/main/java/ru/otus/chat/server/Server.java
@@ -9,10 +9,17 @@ import java.util.List;
 public class Server {
     private int port;
     private List<ClientHandler> clients;
+    private AuthenticatedProvider authenticatedProvider;
 
     public Server(int port) {
         this.port = port;
         clients = new ArrayList<>();
+        authenticatedProvider = new InMemoryAuthenticationProvider(this);
+        authenticatedProvider.initialize();
+    }
+
+    public AuthenticatedProvider getAuthenticatedProvider() {
+        return authenticatedProvider;
     }
 
     public void start() {
@@ -20,7 +27,7 @@ public class Server {
             System.out.println("Сервер запущен на порту: " + port);
             while (true) {
                 Socket socket = serverSocket.accept();
-                subscribe(new ClientHandler(this, socket));
+                new ClientHandler(this, socket);
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -70,6 +77,14 @@ public class Server {
     public synchronized boolean isUserExists(String username) {
         for (ClientHandler c : clients) {
             if (c.getUsername().equals(username)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public boolean isUsernameBusy(String username) {
+        for (ClientHandler client : clients) {
+            if (client.getUsername().equals(username)) {
                 return true;
             }
         }
